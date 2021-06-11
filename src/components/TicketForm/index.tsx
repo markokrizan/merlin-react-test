7b1b1d1c3c2cdc1dc7a4b5187cee0e4dec467c27
Marko Krizan <markokrizan64@gmail.com>
2021-06-11 19:39:17 +0200

    Add ticket form basic styles

--- src/components/TicketForm/index.tsx
+++ src/components/TicketForm/index.tsx
@@ -2,6 +2,8 @@ import { FC, useState } from "react";
 import useKanbanContext from "../../hooks/useKanbanContext/useKanbanContext";
 import { Column, Ticket } from "../../typings/typings";
 
+import styles from "./ticketForm.module.css";
+
 const TicketForm: FC<{ column: Column; closeModal: () => void }> = ({
   column,
   closeModal,
@@ -40,28 +42,46 @@ const TicketForm: FC<{ column: Column; closeModal: () => void }> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <span className={styles["form--title"]}>New ticket</span>
       <input
+        id="title"
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        placeholder={"Title"}
+        className={styles.input}
       />
       <input
+        id="description"
         type="text"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
+        placeholder={"Description"}
+        className={styles.input}
       />
       <input
+        id="acceptanceCriteria"
         type="text"
         value={acceptanceCriteria}
         onChange={(e) => setAcceptanceCriteria(e.target.value)}
+        placeholder={"Acceptance criteria"}
+        className={styles.input}
       />
       <input
+        id="estimate"
         type="number"
         value={estimate}
         onChange={(e) => setEstimate(e.target.value as any)}
+        placeholder={"Estimate"}
+        className={styles.input}
       />
-      <button type="submit">Save Ticket</button>
+      <button type="submit" className={styles.button}>
+        Save Ticket
+      </button>
+      <button className={styles.button} onClick={closeModal}>
+        Close
+      </button>
     </form>
   );
 };
